50d5828a7e6f853f92cece6ef7a5e709d4a7384a
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-05-01 09:39:40 -0600

    changes to commentRoutes

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -3,13 +3,16 @@ const { Comment } = require("../../models/");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
+  if(!req.session.user_id) {
+    return res.status(401).json({ msg: "Please log in to comment." });
+  } else {
   try {
-  const newComment = await Comment.create({ ...req.body, 
-    user_id: req.session.user_id })
+  const newComment = await Comment.create({ ...req.body, })
       if (newComment) res.json(newComment);
   } catch (err) {
     console.log(err)
   }
+}
 });
 
 router.get("/", withAuth, async (req, res) => {

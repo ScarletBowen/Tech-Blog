da6579117ad3af3609993eec734a5cb3d719657c
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-05-03 23:22:53 -0600

    fixed comments rendering

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -7,7 +7,7 @@ router.post("/", async (req, res) => {
     return res.status(401).json({ msg: "Please log in to comment." });
   } else {
   try {
-  const newComment = await Comment.create({ ...req.body, userId: req.session.user_id});
+  const newComment = await Comment.create({ ...req.body, user_id: req.session.user_id});
       if (newComment) res.json(newComment);
   } catch (err) {
     console.log(err)
